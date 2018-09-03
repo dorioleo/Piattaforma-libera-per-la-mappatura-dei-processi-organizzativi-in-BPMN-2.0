4294febbbfe038c1c67810c0418c52876d2ec7e0
Dorio Leonardo <32564984+dorioleo@users.noreply.github.com>
2018-09-03 09:54:39 +0200

    Update popInfo.php

--- popInfo.php
+++ popInfo.php
@@ -10,14 +10,14 @@
 
     <title>BPMN</title>
     <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon"/>
+    
     <!-- Bootstrap core CSS -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Custom styles -->
-	<script src="../bootstrap/jquery/jquery.min.js"></script>    
-	<link rel="stylesheet" href="../bootstrap/css/small-business.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./small-business.css">
+    <link rel="stylesheet" href="./style.css">
+
 	
   </head>
 
